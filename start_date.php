298f35fc2c1eb47af97be44215ea3d713f1692c9
97-42 <144976307+97-42@users.noreply.github.com>
2024-06-17 07:09:16 +0800

    Create start_date.php

---
+++ start_date.php
@@ -0,0 +1,21 @@
+<?php
+$file = 'start_date.txt';
+
+// Handle reset request
+if (isset($_GET['reset'])) {
+    $new_start_date = date('Y-m-d');
+    file_put_contents($file, $new_start_date);
+    echo 'Timer reset';
+    exit;
+}
+
+// Read the current start date from the file
+if (file_exists($file)) {
+    $start_date = file_get_contents($file);
+} else {
+    $start_date = date('Y-m-d');
+    file_put_contents($file, $start_date);
+}
+
+echo $start_date;
+?>
